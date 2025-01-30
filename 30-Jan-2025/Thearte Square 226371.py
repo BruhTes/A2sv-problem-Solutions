145f521d2e11ec4f3823f8bbc959fa21e68e317f
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-01-30 14:46:59 +0300

    Created 'Thearte Square' by A2SV Hub

---
+++ 30-Jan-2025/Thearte Square 226371.py
@@ -0,0 +1,17 @@
+# Problem: Thearte Square - https://codeforces.com/problemset/problem/1/A
+
+import sys
+n = list(map(int, input().split(" ")))
+
+x, y = 0, 0
+if n[0] % n[2] == 0:
+    x = n[0] // n[2]
+else:
+    x = n[0] // n[2] + 1
+
+if n[1] % n[2] == 0:
+    y = n[1] // n[2]
+else:
+    y = n[1] // n[2] + 1
+
+print(x * y)
