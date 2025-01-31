828d15f958d7b66b67fd895dbc17adf3cafb170c
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-01-31 18:25:09 +0300

    Created 'Sum' by A2SV Hub

---
+++ 31-Jan-2025/Sum 231404.py
@@ -0,0 +1,11 @@
+# Problem: Sum - https://codeforces.com/contest/1742/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    a, b, c = map(int, input().split())
+
+    if a + b == c or b + c == a or a + c == b:
+        print("YES")
+    else:
+        print("NO")
