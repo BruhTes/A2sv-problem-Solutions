cd1679407586ae9b4b84383fcf8a19dbfb511d33
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-01-31 18:24:52 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 31-Jan-2025/Love Story 231401.py
@@ -0,0 +1,14 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+t = int(input())
+s1 = "codeforces"
+
+for _ in range(t):
+    s2 = input()
+    count = 0
+
+    for i in range(10):
+        if s1[i] != s2[i]:
+            count += 1
+        
+    print(count)
