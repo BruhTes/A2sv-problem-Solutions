3b180affd202d983998a894bcda5742df988ec49
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-02-01 20:33:49 +0300

    Created 'Powers' by A2SV Hub

---
+++ 01-Feb-2025/Powers 233725.py
@@ -0,0 +1,11 @@
+# Problem: Powers - https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+
+import math
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a,b))
+print(pow(a,b,m))
