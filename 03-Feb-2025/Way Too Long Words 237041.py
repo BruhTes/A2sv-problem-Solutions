aa64bf439a4fc0d88e94c943316af6fe27d58b9a
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-02-03 20:34:22 +0300

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 03-Feb-2025/Way Too Long Words 237041.py
@@ -0,0 +1,17 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+n = int(input())
+
+for _ in range(n):
+    string = input()
+    
+    if len(string) > 10:
+        ls = []
+        ls.append(string[0])
+        ls.append(str(len(string) - 2))
+        ls.append(string[-1])
+        
+        result = "".join(ls)
+        print(result)
+    else:
+        print(string)
