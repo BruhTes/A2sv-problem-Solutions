44dcddea1a737316ce02ef0e145f8cb7d9efbf51
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-02-05 21:45:34 +0300

    Created 'Presents' by A2SV Hub

---
+++ 05-Feb-2025/Presents 244884.py
@@ -0,0 +1,17 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+def presents(friends):
+    result = [None] * len(friends)
+
+    for pi, i in enumerate(friends):
+        result[i - 1] = pi + 1
+
+    return " ".join(map(str, result))
+
+def main():
+    t = int(input().strip())
+    friends = list(map(int, input().split()))
+    print(presents(friends))
+
+if __name__ == "__main__":
+    main()
