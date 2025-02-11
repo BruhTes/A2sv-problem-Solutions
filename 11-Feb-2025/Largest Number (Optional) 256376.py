63b7bff3633ec83703ac55081fa0f5824eee5c33
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-02-11 14:24:48 +0300

    Created 'Largest Number (Optional)' by A2SV Hub

---
+++ 11-Feb-2025/Largest Number (Optional) 256376.py
@@ -0,0 +1,12 @@
+# Problem: Largest Number (Optional) - https://leetcode.com/problems/largest-number/
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        arr = [str(x) for x in nums]
+        arr.sort(key=lambda x:x*10, reverse = True)
+
+        if arr[0] == "0":
+            return "0"
+
+        res = "".join(arr)
+        return res
