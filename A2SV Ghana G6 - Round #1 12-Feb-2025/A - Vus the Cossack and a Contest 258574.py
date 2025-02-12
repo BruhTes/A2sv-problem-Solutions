8638b8b47ee191c13c8d47828859e4c5e286d3b0
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-02-12 04:58:03 +0300

    Created 'A - Vus the Cossack and a Contest' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #1 12-Feb-2025/A - Vus the Cossack and a Contest 258574.py
@@ -0,0 +1,9 @@
+# Problem: A - Vus the Cossack and a Contest - https://codeforces.com/gym/585132/problem/A
+
+n, m, k = list(map(int, input().split()))
+
+
+if n > m or n > k:
+    print("NO")
+else:
+    print("YES")
