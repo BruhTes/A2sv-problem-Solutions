8ba0ae72c64a0c119795818e032addbeb6932956
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-02-12 04:59:17 +0300

    Created 'B - Square String?' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #1 12-Feb-2025/B - Square String? 258575.py
@@ -0,0 +1,22 @@
+# Problem: B - Square String? - https://codeforces.com/gym/585132/problem/B
+
+t = int(input())
+
+for _ in range(t):
+    string = input()
+    if len(string) % 2 != 0:
+        print("NO")
+    else:
+        if len(string) == 2:
+            if string[0] == string[1]:
+                print("YES")
+            else:
+                print("NO")
+
+        elif (string[0:len(string) // 2] == string[len(string) // 2 : len(string)]):
+            print("YES")
+        else:
+            print("NO")
+
+
+
