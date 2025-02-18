5fb93536eeaa65d01e5e9b6d09f3174fcc6ab48c
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-02-18 16:29:22 +0300

    Created 'Car Pooling' by A2SV Hub

---
+++ 18-Feb-2025/Car Pooling 271893.py
@@ -0,0 +1,18 @@
+# Problem: Car Pooling - https://leetcode.com/problems/car-pooling/
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        maxLen = 10000
+        indexUsage = [0] * (maxLen + 1)
+    
+        for numP, start, end in trips:
+            indexUsage[start] += numP
+            if end < len(indexUsage):
+                indexUsage[end] -= numP
+        
+        for i in range(len(indexUsage)):
+            indexUsage[i] += indexUsage[i - 1] if i - 1 >= 0 else 0
+            if indexUsage[i] > capacity:
+                return False
+        print(indexUsage)
+        return True
