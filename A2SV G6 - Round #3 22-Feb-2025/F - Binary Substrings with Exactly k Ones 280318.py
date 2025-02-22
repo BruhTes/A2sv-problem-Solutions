354e84ba1e7b2a410fd2ca746f4eccc4d212fc28
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-02-22 18:13:43 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 22-Feb-2025/F - Binary Substrings with Exactly k Ones 280318.py
@@ -0,0 +1,19 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+k = int(input())
+binary = input()
+
+oneMap = {0 : 1}
+onesCount = 0
+count = 0
+
+for char in binary:
+    if char == "1":
+        onesCount += 1
+    
+    if onesCount - k in oneMap:
+        count += oneMap[onesCount - k]
+    
+    oneMap[onesCount] = oneMap.get(onesCount, 0) + 1
+
+print(count)
