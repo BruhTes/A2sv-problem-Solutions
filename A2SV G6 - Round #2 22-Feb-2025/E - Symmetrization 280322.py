030ae59ee372f6299122104a86f327726497092b
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-02-22 18:15:39 +0300

    Created 'E - Symmetrization' by A2SV Hub

---
+++ A2SV G6 - Round #2 22-Feb-2025/E - Symmetrization 280322.py
@@ -0,0 +1,22 @@
+# Problem: E - Symmetrization - https://codeforces.com/gym/586960/problem/E
+
+for _ in range(int(input())):
+    n = int(input())
+    mat = [list(map(int, input().strip())) for _ in range(n)]
+    
+    mat9 = [list(x) for x in zip(*mat[::-1])]
+    mat18 = [list(x) for x in zip(*mat9[::-1])]
+    mat27 = [list(x) for x in zip(*mat18[::-1])]
+
+    count = 0
+
+    for i in range(n):
+        for j in range(n):
+            total = mat[i][j] + mat9[i][j] + mat18[i][j] + mat27[i][j]
+            
+            if total == 1 or total == 3:
+                count += 1
+            elif total == 2:
+                count += 2
+
+    print(count // 4)
