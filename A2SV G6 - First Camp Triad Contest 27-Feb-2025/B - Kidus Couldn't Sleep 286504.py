8604592a1b2ec59377baedef5f75e3811e637db1
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-02-27 23:29:44 +0300

    Created 'B - Kidus Couldn't Sleep' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 27-Feb-2025/B - Kidus Couldn't Sleep 286504.py
@@ -0,0 +1,21 @@
+# Problem: B - Kidus Couldn't Sleep - https://codeforces.com/gym/589822/problem/B
+
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+
+windowSum = 0
+for i in range(k):
+    windowSum += nums[i]
+
+totalSum = windowSum
+left = 0
+right = k
+
+while right < len(nums):
+    windowSum += nums[right] - nums[left]
+    totalSum += windowSum
+    right += 1
+    left += 1
+
+res = totalSum / (n - k + 1)
+print(f"{res:.10f}")
