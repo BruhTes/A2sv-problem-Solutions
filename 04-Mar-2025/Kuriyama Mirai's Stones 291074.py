d23f7dda96990994e923ee66e509fc2ed4b4e385
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-03-04 16:21:08 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 04-Mar-2025/Kuriyama Mirai's Stones 291074.py
@@ -0,0 +1,20 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+n = int(input())
+nums = list(map(int, input().split()))
+nums2 = sorted(nums)
+
+for i in range(1, n):
+    nums[i] = nums[i] + nums[i - 1]
+    nums2[i] = nums2[i] + nums2[i - 1]
+
+q = int(input())
+for _ in range(q):
+    t, l, r = map(int, input().split())
+
+    if t == 1:
+        Sum = nums[r - 1] - (nums[l - 2] if l > 1 else 0)
+        print(Sum)
+    else:
+        Sum = nums2[r - 1] - (nums2[l - 2] if l > 1 else 0)
+        print(Sum)
