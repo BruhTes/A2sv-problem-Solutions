e0e56611e758686a59efc45a4f29ea203597b826
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-03-06 01:19:32 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 05-Mar-2025/Array Splitting 293791.py
@@ -0,0 +1,18 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+def arraySplit():
+   n, k = map(int, input().split())
+   a = list(map(int, input().split()))
+
+# splitting between two numbers saves you their difference
+   diff = [a[i-1] - a[i] for i in range(1, n)]    
+   diff.sort()
+   res = a[-1] - a[0]
+
+# subtract (k - 1) the max diff saved
+   for i in range(k - 1):
+      res += diff[i]
+    
+   return res
+
+print(arraySplit())
