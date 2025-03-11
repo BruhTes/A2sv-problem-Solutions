a22c7151007e7e52a690833648f07048857cd6e4
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-03-11 17:21:51 +0300

    Created 'E - Minimum Subsequence' by A2SV Hub

---
+++ A2SV G6 - Round #6 11-Mar-2025/E - Minimum Subsequence 302612.py
@@ -0,0 +1,32 @@
+# Problem: E - Minimum Subsequence - https://codeforces.com/gym/594077/problem/E
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    nums = input()
+
+    temp = 1
+    stackZero = []
+    stackOne = []
+    result = []
+
+    for char in nums:
+        if char == '0':
+            if stackOne:
+                result.append(stackOne[-1])
+                stackZero.append(stackOne.pop())
+            else:
+                stackZero.append(temp)
+                result.append(temp)
+                temp += 1
+        else:
+            if stackZero:
+                result.append(stackZero[-1])
+                stackOne.append(stackZero.pop())
+            else:
+                stackOne.append(temp)
+                result.append(temp)
+                temp += 1
+    print(len(stackZero) + len(stackOne))
+    print(*result)        
