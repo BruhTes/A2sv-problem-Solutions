2df110b615d790605b220fb0e268a5173556f840
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-04-04 23:06:32 +0300

    Created 'Masha and a Beautiful Tree' by A2SV Hub

---
+++ 04-Apr-2025/Masha and a Beautiful Tree 326472.py
@@ -0,0 +1,29 @@
+# Problem: Masha and a Beautiful Tree - https://codeforces.com/problemset/problem/1741/D
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    nums = list(map(int, input().split()))
+    count = 0
+
+    def mergeSort(left, right):
+        global count
+        if left == right:
+            return [nums[left]]
+        
+        mid = left + (right - left) // 2
+
+        left_sorted = mergeSort(left, mid)
+        right_sorted = mergeSort(mid + 1, right)
+
+        # do the ckecks here
+        if left_sorted[0] < right_sorted[0]:
+            return left_sorted + right_sorted
+        else:
+            count += 1
+            return right_sorted + left_sorted
+    
+    if mergeSort(0, len(nums) - 1) == sorted(nums):
+        print(count)
+    else:
+        print(-1)
