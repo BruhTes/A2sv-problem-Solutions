87eb23ef8842d0af46a4a9ceb4ee543d822f6315
Biruh Tesfahun <bruhtesfahun21129@gmail.com>
2025-04-24 17:47:59 +0300

    Created 'Set Mismatch' by A2SV Hub

---
+++ 24-Apr-2025/Set Mismatch 341379.py
@@ -0,0 +1,17 @@
+# Problem: Set Mismatch - https://leetcode.com/problems/set-mismatch/description/
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        i = 0
+
+        while i < len(nums):
+            correct = nums[i] - 1
+
+            if nums[i] != nums[correct]:
+                nums[correct], nums[i] = nums[i], nums[correct]
+            else:
+                i += 1
+            
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return [nums[i], i + 1]
